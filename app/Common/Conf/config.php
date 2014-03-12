930b8dc77712819307f464eccadb96169c11b7fc
redspear119 <820534834@qq.com>
2014-03-12 15:25:16 +0800

    更新数据库地址

--- app/Common/Conf/config.php
+++ app/Common/Conf/config.php
@@ -7,10 +7,10 @@ return array(
 
 	//数据库
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_HOST'               =>  '192.168.1.146', // 服务器地址
     'DB_NAME'               =>  'cophea2',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '1234',          // 密码
+    'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  3306,        // 端口
     'DB_PREFIX'             =>  'coph_',    // 数据库表前缀
 	
